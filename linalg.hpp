b93ca8790d9d1f9a53825dba94e5039ae7fc5af4
tmarcato <tmarcato@ethz.ch>
2024-04-04 11:25:52 +0200

    Added scalar multiplication for std::vector by overloading

--- linalg.hpp
+++ linalg.hpp
@@ -7,6 +7,24 @@ Generic library for vector utilities. It is based on the std::vector container.
 #include<vector>
 #include<cassert>
 
+// Vector arithmetic
+// Scalar Multiplication
+template<typename T, typename U>
+std::vector<T>& operator*(std::vector<T>& vec, const U scalar) {
+    for (size_t i=0; i<vec.size(); ++i) {
+        vec[i] *= scalar;
+    }
+    return vec;
+}
+
+template<typename T, typename U>
+std::vector<T>& operator*(const U scalar, std::vector<T>& vec) {
+    for (size_t i=0; i<vec.size(); ++i) {
+        vec[i] *= scalar;
+    }
+    return vec;
+}
+
 template<typename T>
 void arange(std::vector<T>& out, T start, T stop, T step) {
 
--- main.cpp
+++ main.cpp
@@ -6,4 +6,11 @@ int main(int argc, char* argv[]) {
     std::vector<double> v;
     linspace(v, 1.1, 2.0, 10.0);
     print(v);
+    std::cout << "\n";
+    // Test scalar multiplication
+    v * 2.0;
+    print(v);
+    std::cout << "\n";
+    2.0 * v;
+    print(v);
 }
