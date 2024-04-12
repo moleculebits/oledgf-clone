7a9a96a58690175a374b48cea3023cbc89b57a9f
tmarcato <tmarcato@ethz.ch>
2024-04-12 13:24:25 +0200

    Added global .clang-format for formatting options

--- linalg.hpp
+++ linalg.hpp
@@ -3,99 +3,87 @@ Generic library for vector utilities. It is based on the std::vector container.
 */
 #pragma once
 
-#include<iostream>
-#include<vector>
-#include<cmath>
+#include <cmath>
+#include <iostream>
+#include <vector>
 
-#include<massert.hpp>
+#include <massert.hpp>
 
 // Vector arithmetic
 // Scalar Multiplication
-template<typename T, typename U>
-std::vector<T> operator*(const std::vector<T>& vec, const U scalar) {
-    std::vector<T> res;
-    res.reserve(vec.size());
+template<typename T, typename U> std::vector<T> operator*(const std::vector<T>& vec, const U scalar)
+{
+  std::vector<T> res;
+  res.reserve(vec.size());
 
-    for (auto& elem: vec) {
-        res.push_back(scalar * elem);
-    }
-    return res;
+  for (auto& elem : vec) { res.push_back(scalar * elem); }
+  return res;
 }
 
-template<typename T, typename U>
-std::vector<T> operator*(const U scalar, const std::vector<T>& vec) {
-    std::vector<T> res;
-    res.reserve(vec.size());
+template<typename T, typename U> std::vector<T> operator*(const U scalar, const std::vector<T>& vec)
+{
+  std::vector<T> res;
+  res.reserve(vec.size());
 
-    for (auto& elem: vec) {
-        res.push_back(scalar * elem);
-    }
-    return res;
+  for (auto& elem : vec) { res.push_back(scalar * elem); }
+  return res;
 }
 
 // Vector addition
-template<typename T>
-std::vector<T> operator+(const std::vector<T>& lvec, const std::vector<T>& rvec) {
-    m_assert(lvec.size() == rvec.size(), "Only vectors with same size can be added!");
-    std::vector<T> res;
-    res.reserve(lvec.size());
+template<typename T> std::vector<T> operator+(const std::vector<T>& lvec, const std::vector<T>& rvec)
+{
+  m_assert(lvec.size() == rvec.size(), "Only vectors with same size can be added!");
+  std::vector<T> res;
+  res.reserve(lvec.size());
 
-    for (size_t i=0; i<lvec.size(); ++i) {
-        res.push_back(lvec[i] + rvec[i]);
-    }
-    return res;
+  for (size_t i = 0; i < lvec.size(); ++i) { res.push_back(lvec[i] + rvec[i]); }
+  return res;
 }
 
 // Vector element-wise multiplication
-template<typename T>
-std::vector<T> operator*(const std::vector<T>& lvec, const std::vector<T>& rvec) {
-    m_assert(lvec.size() == rvec.size(), "Only vector of same size can be multiplied together element-wise!");
-    std::vector<T> res;
-    res.reserve(lvec.size());
+template<typename T> std::vector<T> operator*(const std::vector<T>& lvec, const std::vector<T>& rvec)
+{
+  m_assert(lvec.size() == rvec.size(), "Only vector of same size can be multiplied together element-wise!");
+  std::vector<T> res;
+  res.reserve(lvec.size());
 
-    for (size_t i=0; i<lvec.size(); ++i) {
-        res.push_back(lvec[i] * rvec[i]);
-    }
-    return res;
+  for (size_t i = 0; i < lvec.size(); ++i) { res.push_back(lvec[i] * rvec[i]); }
+  return res;
 }
 
 // Vector slicing
-template<typename T>
-std::vector<T> slice(const std::vector<T>& vec, size_t start, int end, size_t stride=0) {
-    m_assert((size_t)std::abs(end) <= vec.size(), "Slice upper bound is out of range!");
+template<typename T> std::vector<T> slice(const std::vector<T>& vec, size_t start, int end, size_t stride = 0)
+{
+  m_assert((size_t)std::abs(end) <= vec.size(), "Slice upper bound is out of range!");
 
-    std::vector<T> res;
-    size_t upperBound = end >= 0 ? end : vec.size() + end;
-    res.reserve((upperBound - start)/stride);
+  std::vector<T> res;
+  size_t upperBound = end >= 0 ? end : vec.size() + end;
+  res.reserve((upperBound - start) / stride);
 
-    for(size_t i=start; i<upperBound; i+=stride) {
-        if (i>=vec.size()) break;
-        res.push_back(vec[i]);
-    }
-    return res;
+  for (size_t i = start; i < upperBound; i += stride) {
+    if (i >= vec.size()) break;
+    res.push_back(vec[i]);
+  }
+  return res;
 }
 
-template<typename T>
-void arange(std::vector<T>& out, T start, T stop, T step) {
+template<typename T> void arange(std::vector<T>& out, T start, T stop, T step)
+{
 
-    m_assert(stop > start, "Upper bound has to be greater than lower bound!");
-    size_t N = ((stop - start) / step) + 1;
-    out.reserve(N);
+  m_assert(stop > start, "Upper bound has to be greater than lower bound!");
+  size_t N = ((stop - start) / step) + 1;
+  out.reserve(N);
 
-    for (size_t i=0; i<N; ++i) {
-        out.push_back(start + i*step);
-    }
+  for (size_t i = 0; i < N; ++i) { out.push_back(start + i * step); }
 }
 
-template<typename T>
-void linspace(std::vector<T>& out, T start, T stop, size_t N) {
-    T step = (stop - start) / (N - 1);
-    arange(out, start, stop, step);
+template<typename T> void linspace(std::vector<T>& out, T start, T stop, size_t N)
+{
+  T step = (stop - start) / (N - 1);
+  arange(out, start, stop, step);
 }
 
-template<typename T>
-void print(const std::vector<T>& vec) {
-    for (auto& elem: vec) {
-        std::cout << elem << "\n";
-    }
+template<typename T> void print(const std::vector<T>& vec)
+{
+  for (auto& elem : vec) { std::cout << elem << "\n"; }
 }
--- material.cpp
+++ material.cpp
@@ -1,21 +1,19 @@
-#include<algorithm>
-#include<material.hpp>
-#include<files.hpp>
+#include <algorithm>
+#include <files.hpp>
+#include <material.hpp>
 
-Material::Material(double realRefIndex) {
-    mRefIndex.push_back(realRefIndex);
+Material::Material(double realRefIndex) { mRefIndex.push_back(realRefIndex); }
+
+Material::Material(double realRefIndex, double imagRefIndex)
+{
+  mRefIndex.push_back(realRefIndex);
+  mRefIndex.push_back(imagRefIndex);
 }
 
-Material::Material(double realRefIndex, double imagRefIndex) {
-    mRefIndex.push_back(realRefIndex);
-    mRefIndex.push_back(imagRefIndex);
+Material::Material(const std::filesystem::path& path, const char delimiter)
+{
+  MFile mFile = MFile(path, delimiter);
+  mRefIndex = mFile.getData();
 }
 
-Material::Material(const std::filesystem::path& path, const char delimiter) {
-    MFile mFile = MFile(path, delimiter);
-    mRefIndex = mFile.getData();
-}
-
-std::vector<double> Material::getRefIndex() const{
-    return mRefIndex;
-}
+std::vector<double> Material::getRefIndex() const { return mRefIndex; }
